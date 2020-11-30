1255cd047fe96afb7683c5166733a2ea47b40b16
Evgeniy Tarasov <tarasovem@gmail.com>
2020-11-30 19:24:57 +0300

    Выносит функцию подсчета фильмов из шаблона и упрощает ее

--- src/view/menu.js
+++ src/view/menu.js
@@ -1,26 +1,22 @@
-export const menuTemplate = (films) => {
-  let watchListCount = 0;
-  let historyCount = 0;
-  let favoritesCount = 0;
+const getCount = (list, condition) => {
+  let count = 0;
 
-  for (let film of films) {
-    if (film.isAddedToWatchlist) {
-      watchListCount += 1;
-    }
-    if (film.isWatched) {
-      historyCount += 1;
-    }
-    if (film.isFavorite) {
-      favoritesCount += 1;
+  for (const item of list) {
+    if (item[condition]) {
+      count += 1;
     }
   }
 
+  return count;
+};
+
+export const menuTemplate = (films) => {
   return `<nav class="main-navigation">
     <div class="main-navigation__items">
       <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${watchListCount}</span></a>
-      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${historyCount}</span></a>
-      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${favoritesCount}</span></a>
+      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${getCount(films, `isAddedToWatchlist`)}</span></a>
+      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${getCount(films, `isWatched`)}</span></a>
+      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${getCount(films, `isAddedToWatchlist`)}</span></a>
     </div>
     <a href="#stats" class="main-navigation__additional">Stats</a>
   </nav>
